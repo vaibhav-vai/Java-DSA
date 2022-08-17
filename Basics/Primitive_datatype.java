a0ae3c77069f43f817452cf3031c877c0a322778
Vaibhav <98206149+vaibhav-vai@users.noreply.github.com>
2022-08-17 23:03:31 +0530

    Create Primitive_datatype.java

---
+++ Basics/Primitive_datatype.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		int number = 665;
+		char letter = 'a';
+		float decimal = 99.999f;
+		double largeDecimal = 45684521.654664984;
+		long largeInteger = 51654156451568L;
+		boolean check = false;
+		System.out.println(number + "\n" + letter + "\n" + decimal + "\n" + largeDecimal + "\n" + largeInteger + "\n" + check);
+	}
+}
+
