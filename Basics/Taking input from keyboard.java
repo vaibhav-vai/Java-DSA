50ad2d539209ebebcf857d7bab9166c877c45a28
Vaibhav <98206149+vaibhav-vai@users.noreply.github.com>
2022-08-17 19:10:48 +0530

    Update Taking input from keyboard.java

--- Basics/Taking input from keyboard.java
+++ Basics/Taking input from keyboard.java
@@ -3,6 +3,8 @@ public class Main
 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.println(input.nextLine());
+		System.out.print("Please enter your roll no. :");
+		int rollno = input.nextInt();
+		System.out.println("Your roll no. is" + rollno);
 	}
 }
