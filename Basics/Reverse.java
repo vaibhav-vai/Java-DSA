5ce0579aa4b28e924010eb1a4d2ea793b4dc86c7
Vaibhav <98206149+vaibhav-vai@users.noreply.github.com>
2022-08-20 16:53:23 +0530

    Add files via upload

---
+++ Basics/Reverse.java
@@ -0,0 +1,13 @@
+public class Reverse {
+    public static void main(String[] args) {
+        int num = 1635498;
+        int ans = 0;
+        while (num >0){
+            int rem = num % 10;
+            ans = ans * 10 + rem;
+            num /= 10;
+        }
+        System.out.println(ans);
+    }
+    
+}
