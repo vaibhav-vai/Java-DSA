e9e3246300b179ed7f827abe6ff498a44e18448d
Vaibhav <98206149+vaibhav-vai@users.noreply.github.com>
2022-09-17 12:57:27 +0530

    Add files via upload

---
+++ Codes/Functions/Factorial.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Factorial{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        printFactorial(n);
+    }
+
+
+public static void printFactorial(int n){
+    int factorial = 1;
+    //loop
+    for (int i=n; i>=1 ; i--){
+        factorial = factorial *i;
+    }
+    System.out.println(factorial);
+    return;
+
+    }
+}
