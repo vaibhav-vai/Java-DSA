23832f20290a33ee8c9859761c6442b337f1f5b4
Vaibhav <98206149+vaibhav-vai@users.noreply.github.com>
2022-10-06 18:36:14 +0530

    Update 07Reverse.java

--- Codes/03Arrays/07Reverse.java
+++ Codes/03Arrays/07Reverse.java
@@ -26,3 +26,22 @@ public class Reverse {
         arr[index2] = temp;
      }
 }
+
+// Or 
+
+/*
+int[] arr = {1,2,3,4,5,6};
+int l = arr.length;
+int n = Math.floorDiv(1,2);
+
+for(int i = 0 ; i<n ; i ++){
+    //swap a[i] and a[l-1-i] 
+    temp = arr[i];
+    arr[i] = arr[l-1-i];
+    arr[l-1-i] = temp;
+} 
+
+for(int element: arr){
+    System.out.print(element + "");
+}
+ */
